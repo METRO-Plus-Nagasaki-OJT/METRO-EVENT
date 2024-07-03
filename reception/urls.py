b604adc40fced24c53669e8ffaa646b42ad21680
DevWaiYanLinn <waiyanlinn.lion@outlook.com>
2024-07-03 16:06:26 +0900

    update naming

--- reception/urls.py
+++ reception/urls.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.urls import path
+from . import views
+app_name = "reception"
 urlpatterns = [
-    
+    path("", views.index, name="reception")
 ]
