281025d0eaf6cb21a674012f6af4bb45ec9610b2
XxMika <aungphonekhant187@gmail.com>
2024-07-08 17:16:50 +0900

    changes for participant, added camera function, events select from database

--- participant/models.py
+++ participant/models.py
@@ -8,7 +8,7 @@ class Participant(models.Model):
     seat_no = models.CharField(max_length=255)
     gender = models.BooleanField()
     profile = models.TextField(default=None)
-    dob = models.DateField()
+    dob = models.DateField()    
     email = models.EmailField()
     face = models.BooleanField()
     facial_feature = models.TextField(default=None)
