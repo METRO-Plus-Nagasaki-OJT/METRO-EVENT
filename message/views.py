da4abab334a07dc28002bb9268e5b660edc22bda
HeinSit <learningmaster119@gmail.com>
2024-07-09 15:17:59 +0900

    Creating Message

--- message/views.py
+++ message/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def message_view(request):
+    return render(request, 'message/message.html')
--- p_hub/settings.py
+++ p_hub/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'event',
     'attendance',
     'reception',
+    'message',
  
 ]
 
