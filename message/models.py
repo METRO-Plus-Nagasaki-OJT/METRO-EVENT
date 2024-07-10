07c68f92a87fd72c23d8570a555b892a7e48be4f
HeinSit <learningmaster119@gmail.com>
2024-07-10 13:12:33 +0900

    Creation of Message

--- message/models.py
+++ message/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    sender = models.CharField(max_length = 255)
+    subject = models.CharField(max_length = 255)
+    period = models.DateField()
+    situation = models.CharField(max_length = 255)
--- message/views.py
+++ message/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from .models import Message
+from django.http import JsonResponse
 
 # Create your views here.
 def message_view(request):
+    if request.method == "POST":
+        sender = request.POST.get('sender')
+        subject = request.POST.get('subject')
+        period = request.POST.get('period')
+        situation = request.POST.get('situation')
+
+        message = Message(
+            sender = sender,
+            subject = subject,
+            period = period,
+            situation = situation
+        )
+        message.save()
+        
+        return JsonResponse({'stauts': 'success', 'message': "Message have been created successfully!"})
+    
+    elif request.method == "GET":
+        messages = Message.objects.all()
+
     return render(request, 'message/message.html')
