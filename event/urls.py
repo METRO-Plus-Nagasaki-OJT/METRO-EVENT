6463c730c51c0102d7f53068206277ac3a6560f6
HtetMyetTun <htetmyettun223@gmail.com>
2024-07-11 17:50:04 +0900

    set value

--- event/urls.py
+++ event/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path("",views.index,name="event"),
+    path('deleteevent/<int:id>/', views.delete, name='delete'),
     path("updateevent/<int:id>/",views.edit,name="update")
 ]
