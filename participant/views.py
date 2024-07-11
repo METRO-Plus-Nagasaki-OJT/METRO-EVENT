bbcae963ed85e3527e5ca0b89a6c4a03d1d71f8a
XxMika <aungphonekhant187@gmail.com>
2024-07-11 12:52:43 +0900

    fix-model

--- participant/views.py
+++ participant/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
-from .models import Participant  
+from django.shortcuts import render, get_object_or_404
+from .models import Participant
 from event.models import Event
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import render, get_object_or_404
 
 
 def participant(request):
@@ -19,6 +18,10 @@ def participant(request):
         phone_2 = request.POST.get('phone_2')
         memo = request.POST.get('memo')
         address = request.POST.get('address')
+        event_id = request.POST.get('event') 
+
+        print(event_id)
+        event = get_object_or_404(Event, id=event_id)
 
         # Create and save the Participant object
         participant = Participant(
@@ -28,10 +31,11 @@ def participant(request):
             dob=dob,
             gender=gender,
             role=role,
-            phone1=phone_1,
-            phone2=phone_2,
+            phone_1=phone_1,
+            phone_2=phone_2,
             memo=memo,
-            address=address
+            address=address,
+            event=event  
         )
         participant.save()
 
@@ -52,8 +56,8 @@ def get_participant_data(request, participant_id):
             'email': participant.email,
             'seat_no': participant.seat_no,
             'dob': participant.dob,
-            'phone1': participant.phone_1,
-            'phone2': participant.phone_2,
+            'phone_1': participant.phone_1,
+            'phone_2': participant.phone_2,
             'memo': participant.memo,
             'address': participant.address,
             'role': participant.role,
@@ -61,4 +65,3 @@ def get_participant_data(request, participant_id):
             # 'image_url': participant.image_url,  # Assuming you have this field in your model
         }
         return JsonResponse(data)
-
