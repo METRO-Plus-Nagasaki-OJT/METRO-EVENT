4ada1cae7e2b566a04612de639c82be53feee4ab
DevWaiYanLinn <waiyanlinn.lion@outlook.com>
2024-07-11 15:29:04 +0900

    add-qr

--- resources/js/mediapipe.js
+++ resources/js/mediapipe.js
@@ -1,6 +1,8 @@
 
 import { FaceDetection } from '@mediapipe/face_detection';
 import { Hands } from '@mediapipe/hands';
+import jsQR from "jsqr";
 
 window.FaceDetection = FaceDetection
 window.Hands = Hands
+window.jsQR = jsQR
