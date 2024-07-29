bc156ef319ce1aa9a5158f25662405502dc92fa6
XxMika <aungphonekhant187@gmail.com>
2024-07-29 15:24:19 +0900

    added monitoring

--- p_hub_admin/urls.py
+++ p_hub_admin/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.login_view, name = "login")
+    path("", views.login_view, name = "login"),
+    path('monitoring/', views.monitoring, name='monitoring'),
+    path('participants/<int:event_id>/', views.participants_list, name='participants_list'),
 ]
