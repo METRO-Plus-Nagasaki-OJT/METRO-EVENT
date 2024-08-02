2d250fc73659f298681694fbe227b79ca35892e6
XxMika <aungphonekhant187@gmail.com>
2024-08-02 13:49:01 +0900

    nav bar fix, slide fix

--- attendance/management/commands/create_schedule_attendance.py
+++ attendance/management/commands/create_schedule_attendance.py
@@ -23,14 +23,14 @@ def adding_attendance(next_day):
             Attendance.objects.create(participant_id=participant_id, date=next_day)
 
 def attendance_scheduling():
-    attendance_for_today = Attendance.objects.filter(date=tomorrow).count()
+    attendance_for_today = Attendance.objects.filter(date=today).count()
     if attendance_for_today == 0:
         if current_weekday != 5 or current_weekday != 6:
             if current_weekday == 4:
                 next_monday = today + timedelta(days=3)
-                adding_attendance(next_monday)
+                adding_attendance(today)
             else:
-                adding_attendance(tomorrow)
+                adding_attendance(today)
             print("finished creating")
     else:
         pass
